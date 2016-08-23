4774892db868cd0f369c8a87d949e1fa776586ff
spiraltrain-gituser <gitcourse@spiraltrain.nl>
2016-08-23 20:28:47 +0200

    add method

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.add(1, 1));
+	}
+	
+	int add(int i, int j){
+		return i + j;
+	}
+}
